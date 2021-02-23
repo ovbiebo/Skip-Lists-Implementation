015d0936f84346c6534a95296a46272c7becd31d
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-02-23 12:39:27 +0100

    Implemented observer pattern for UI and added print and clear functionality

--- scripts/skiplist.js
+++ scripts/skiplist.js
@@ -1,47 +1,86 @@
 // Things to think about
 // How to determine the max level of the skip list - user should input or random number between  1-5
-// How to 
+// How to
 
 // TODO
 // for insertion
 // Get head node
 // check if insertion value is greate or less than current node
-// if it's less move to next 
+// if it's less move to next
 //      if next is null move down
-// 
+//
 // if next is null move down
-// 
+//
+
+class Observable {
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(fnToAdd) {
+        this.observers.push(fnToAdd);
+    }
+
+    unsubscribe(fnToRemove) {
+        this.observers = this.observers.filter((fn) => {
+            if (fn != fnToRemove) return fn;
+        });
+    }
+
+    fire(updatedState) {
+        this.observers.forEach((fn) => {
+            if (typeof fn === "function") fn(updatedState);
+        });
+    }
+}
 
 class Node {
-    constructor(value, next, down) {
+    constructor(value, next = null, down = null) {
         this.value = value;
         this.next = next;
         this.down = down;
     }
 }
 
-class SkipList {
-    constructor(head){
-        this.head = new Node();
+class SkipList extends Observable {
+    constructor(head) {
+        super();
+        this.head = new Node(Number.NEGATIVE_INFINITY, null, null);
+        this.tail = new Node(Number.POSITIVE_INFINITY, null, null);
     }
 
-    insert(node) {
-        const input = document.querySelector("#insert");
+    insert(numberToInsert) {
+        super.fire(this.head);
     }
 
-    delete(node) {
-        const input = document.querySelector("#delete");
+    delete(numberToDelete) {
+        super.fire(this.head);
     }
 
-    find(node) {
-        const input = document.querySelector("#find");
+    find(numberToFind) {
+        super.fire(this.head);
     }
 
-    print() {
-        console.log(this.head)
+    toString() {
+        let currentNode = this.head;
+        let skipListAsString = "";
+
+        while (currentNode.down != null) {
+            currentNode = currentNode.down;
+            while (currentNode.right != this.tail) {
+                skipListAsString.concat(`${currentNode.element} `);
+                currentNode = currentNode.right;
+            }
+        }
+
+        return skipListAsString ? `SkipList: ${skipListAsString}` : "";
     }
 
     clear() {
-        this.head = null;
+        this.head.right = this.tail;
+        this.head.down = null;
+        super.fire(this.head);
     }
 }
+
+const skipList = new SkipList(null);
--- scripts/view.js
+++ scripts/view.js
@@ -1,22 +1,73 @@
-document.querySelector("#insertButton").addEventListener('click', insertButtonClicked);
-document.querySelector("#deleteButton").addEventListener('click', deleteButtonClicked);
-document.querySelector("#findButton").addEventListener('click', findButtonClicked);
-document.querySelector("#printButton").addEventListener('click', printButtonClicked);
-document.querySelector("#clearButton").addEventListener('click', clearButtonClicked);
+document
+    .querySelector("#insertButton")
+    .addEventListener("click", insertButtonClicked);
+document
+    .querySelector("#deleteButton")
+    .addEventListener("click", deleteButtonClicked);
+document
+    .querySelector("#findButton")
+    .addEventListener("click", findButtonClicked);
+document
+    .querySelector("#printButton")
+    .addEventListener("click", printButtonClicked);
+document
+    .querySelector("#clearButton")
+    .addEventListener("click", clearButtonClicked);
 
-function insertButtonClicked(c){
+const insertField = document.querySelector("#insert");
+const deleteField = document.querySelector("#delete");
+const findField = document.querySelector("#find");
+
+const skiplistGraphicDisplay = document.querySelector(
+    "#skipListGraphicDisplay"
+);
+const skiplistTextDisplay = document.querySelector("#skipListTextDisplay");
+
+function insertButtonClicked(c) {
+    skipList.insert(insertField.value);
 }
 
-function deleteButtonClicked(c){
+function deleteButtonClicked(c) {
+    skipList.delete(deleteField.value);
 }
 
-function findButtonClicked(c){
+function findButtonClicked(c) {
+    skipList.find(findField.value);
 }
 
-function printButtonClicked(c){
-    const skiplist = new SkipList();
-    skiplist.print();
+function printButtonClicked(c) {
+    write(skipList.toString());
 }
 
-function clearButtonClicked(c){
+function clearButtonClicked(c) {
+    skipList.clear();
+}
+
+skipList.subscribe(drawSkipList);
+
+function drawSkipList(updatedSkiplist) {
+    let currentNode = updatedSkiplist;
+
+    while (currentNode.down != null) {
+        currentNode = currentNode.down;
+        while (currentNode.right != this.tail) {
+            drawNode();
+            skiplistGraphicDisplay.appendChild();
+            currentNode = currentNode.right;
+        }
+    }
+
+    drawNode();
+}
+
+function drawNode(){
+    let newNode = document.createElement("div");
+    let newNodeContent = document.createTextNode(2);
+    newNode.appendChild(newNodeContent);
+    newNode.setAttribute("class", "node");
+    skiplistGraphicDisplay.appendChild(newNode);
+}
+
+function write(skiplist) {
+    skiplistTextDisplay.innerText = skiplist;
 }
